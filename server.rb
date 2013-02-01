b306672f00528630dc7f8162e257d750e428d0a9
Ondrej Bima <ondrej.bima@kraxnet.cz>
2013-02-02 00:05:46 +0100

    working prototype

--- server.rb
+++ server.rb
@@ -2,10 +2,10 @@ require 'sinatra'
 
 TEMP_DIR = "#{File.dirname(__FILE__)}/tmp"
 OUTPUT_MODES = ["hours", "days", "months", "top10", "summary", "hsummary", "vsummary"]
-INTERFACES = ["em0", "re0", "giff0"]
+INTERFACES = ["em0", "re0", "gif0"]
 
 get '/' do
-  "Hello World!"
+  erb :index, :layout => true
 end
 
 get '/img/:interface/:mode.png' do
@@ -13,11 +13,17 @@ get '/img/:interface/:mode.png' do
   output_mode = params[:mode].downcase
   halt 415, "Unknown output mode" unless OUTPUT_MODES.include?(output_mode)
   halt 415, "Unknown interface" unless INTERFACES.include?(interface)
-  # content_type 'image/png'
-  # `vnstati -m -i em0 -o -`
-  generate_image(interface, output_mode)
+  output_file = generate_image_with_stats(interface, output_mode)
+  if output_file
+    content_type 'image/png'
+    send_file output_file
+  else
+    halt 503, "Image missing"
+  end
 end
 
-def generate_image(interface, output_mode)
-  "vnstati -i #{interface} --#{output_mode} -o #{TEMP_DIR}/vnstat-#{interface}-#{output_mode}.png"
+def generate_image_with_stats(interface, output_mode)
+  output_file = "#{TEMP_DIR}/vnstat-#{interface}-#{output_mode}.png"
+  begin `vnstati -i #{interface} --#{output_mode} -o #{output_file}` rescue return false end
+  $? == 0 ? output_file : false
 end
