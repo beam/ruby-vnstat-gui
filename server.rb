e5e13d0e3c777444fb6c20d2d8ff486ea8929f6d
Ondrej Bima <ondrej.bima@kraxnet.cz>
2013-02-03 00:26:03 +0100

    image caching

--- server.rb
+++ server.rb
@@ -3,6 +3,24 @@ require 'sinatra'
 TEMP_DIR = "#{File.dirname(__FILE__)}/tmp"
 OUTPUT_MODES = ["hours", "days", "months", "top10", "summary", "hsummary", "vsummary"]
 INTERFACES = ["em0", "re0", "gif0"]
+CACHE_IMAGE = 5 # 5 minut (as cron)
+
+def build_image_filename(interface, output_mode)
+  return "#{TEMP_DIR}/vnstat-#{interface}-#{output_mode}.png"
+end
+
+def check_image_cache(interface,output_mode)
+  output_file = build_image_filename(interface,output_mode)
+  return false unless File.exists?(output_file)
+  mtime = File.mtime(output_file)
+  return (Time.now - mtime) < ((CACHE_IMAGE - (mtime.min % CACHE_IMAGE)) * 60) # expire cache in 0,5,10..
+end
+
+def generate_image_with_stats(interface, output_mode)
+  output_file = build_image_filename(interface,output_mode)
+  begin `vnstati -i #{interface} --#{output_mode} -o #{output_file}` rescue return false end
+  $? == 0 ? output_file : false
+end
 
 get '/' do
   erb :index, :layout => true
@@ -13,17 +31,11 @@ get '/img/:interface/:mode.png' do
   output_mode = params[:mode].downcase
   halt 415, "Unknown output mode" unless OUTPUT_MODES.include?(output_mode)
   halt 415, "Unknown interface" unless INTERFACES.include?(interface)
-  output_file = generate_image_with_stats(interface, output_mode)
+  output_file = check_image_cache(interface, output_mode) ? build_image_filename(interface,output_mode) : generate_image_with_stats(interface, output_mode)
   if output_file
     content_type 'image/png'
     send_file output_file
   else
     halt 503, "Image missing"
   end
-end
-
-def generate_image_with_stats(interface, output_mode)
-  output_file = "#{TEMP_DIR}/vnstat-#{interface}-#{output_mode}.png"
-  begin `vnstati -i #{interface} --#{output_mode} -o #{output_file}` rescue return false end
-  $? == 0 ? output_file : false
 end
